d0783185ac550cc7e7338575d905952edbbc58d9
Tom Hulihan <hulihan.tom159@gmail.com>
2015-01-20 12:07:38 -0500

    Add tasks to compile scss.

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -4,7 +4,7 @@ var Game = require('./game.jsx');
 var Home = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className="center">
         <h1>Welcome to 2048!</h1>
         <Game/>
       </div>
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var http = require('http');
 var rimraf = require('rimraf');
 var serveStatic = require('serve-static');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 gulp.task('clean', function(cb) {
   rimraf('./build/', cb);
@@ -17,19 +18,25 @@ gulp.task('js', function() {
     .transform('reactify')
     .bundle()
     .pipe(source('deps.min.js'))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('scss', function() {
+  gulp.src('./style/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('html', function() {
-  gulp.src('index.html')
-    .pipe(gulp.dest('build/'));
+  gulp.src('./index.html')
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('build', ['js', 'html']);
+gulp.task('build', ['js', 'scss', 'html']);
 
 gulp.task('serve', ['build'], function() {
   var port = parseInt(process.env.PORT) || 3000;
-  var serve = serveStatic('build/', { index: ['index.html'] })
+  var serve = serveStatic('./build/', { index: ['index.html'] })
   var server = http.createServer(function(req, res){
     var done = finalhandler(req, res)
     serve(req, res, done)
@@ -39,8 +46,9 @@ gulp.task('serve', ['build'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*', ['js']);
-  gulp.watch('index.html', ['html']);
+  gulp.watch('./app/**/*', ['js']);
+  gulp.watch('./index.html', ['html']);
+  gulp.watch('./style/**/*.scss', ['scss']);
 });
 
 gulp.task('default', ['build']);
